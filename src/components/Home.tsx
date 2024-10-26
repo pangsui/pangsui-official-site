73d5d9f2a1dec69e69630b331360937dacfbdc40
pangsui <upangsuilingeh@gmail.com>
2024-10-26 17:55:20 +0200

    home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,26 @@
 function Home() {
-  return <></>;
+  return (
+    <section className="home">
+      <h1>
+        Welcome to <span>Dr. Eng. Pangsui Usifu Linge</span> Official Site
+      </h1>
+      <h2>Professional Summary</h2>
+      <p>
+        Solutions-oriented PhD holder in Electrical Engineering with specialized
+        expertise in front-end development, complex problem-solving, and
+        critical thinking. Proficient in HTML, CSS, JavaScript, and React, with
+        a strong foundation in UI/UX design to craft engaging, user-centric, and
+        responsive web applications. Skilled in Agile methodologies and team
+        collaboration, with a track record of enhancing project outcomes through
+        iterative development, adaptability, and cross-functional teamwork.
+        Known for applying engineering precision, critical thinking, and design
+        sensibility to tackle challenging technical issues and deliver
+        streamlined digital solutions. Seeking a front-end development role to
+        apply my technical expertise, Agile mastery, and problem-solving
+        abilities in a dynamic, growth-oriented environment.
+      </p>
+    </section>
+  );
 }
 
 export default Home;

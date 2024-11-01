42dbf1be716e23f1146c377ceb6ab4076330e603
pangsui <upangsuilingeh@gmail.com>
2024-11-01 21:30:38 +0100

    form submission

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,45 @@
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-function Contact() {
+
+interface FormData {
+  name: string;
+  email: string;
+  message: string;
+}
+
+const Contact: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    // Convert FormData to an array of key-value pairs
+    const formEntries = Array.from(formData.entries());
+
+    // Trigger form submission with Netlify
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formEntries as [string, string][]).toString(),
+    })
+      .then(() => alert("Form submitted successfully!"))
+      .catch((error) => alert(error));
+  };
   return (
     <div className="contact" id="contact">
       <div className="contact-text">
@@ -23,36 +61,39 @@ function Contact() {
         name="contact"
         method="POST"
         data-netlify="true"
-        onSubmit={() => ""}
-        netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
       >
+        <input type="hidden" name="form-name" value="contact" />
         <div className="form-group">
           <label className="visually-hidden">Company Name</label>
           <input
             type="text"
-            id="first-name"
-            name="first-name"
-            placeholder="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
             required
+            placeholder="name"
           />
         </div>
         <div className="form-group">
           <label className="visually-hidden">Email Address</label>
           <input
             type="email"
-            id="email"
             name="email"
-            placeholder="email address"
+            value={formData.email}
+            onChange={handleChange}
             required
+            placeholder="email address"
           />
         </div>
         <div className="form-group">
           <label className="visually-hidden">Message</label>
           <textarea
             name="message"
-            id="message"
-            placeholder="enter message"
+            value={formData.message}
+            onChange={handleChange}
             required
+            placeholder="enter message"
           ></textarea>
         </div>
         <div className="form-group">
@@ -63,6 +104,6 @@ function Contact() {
       </form>
     </div>
   );
-}
+};
 
 export default Contact;

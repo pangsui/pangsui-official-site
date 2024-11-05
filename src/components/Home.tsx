2488403afcb105be171016046eec6ad82d9e2e7d
pangsui <upangsuilingeh@gmail.com>
2024-11-05 17:39:28 +0100

    home and resume modifield

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,17 +8,18 @@ const Home = React.forwardRef<HTMLElement, object>(() => {
       </h1>
       <h2>Professional Summary</h2>
       <p>
-        I am a solution-oriented Front-End Developer with a PhD in Electrical
+        I am a solution-oriented Full Stack Developer with a PhD in Electrical
         Engineering and a strong foundation in web development from Wild Code
         School and the Udemy online platform. Proficient in HTML, CSS,
-        JavaScript, TypeScript, and React, I have completed multiple personal
-        projects that showcase my skills in building dynamic, responsive web
-        applications. Experienced in Agile methodologies and Scrum practices, I
-        am adept at working in iterative, collaborative environments to deliver
-        high-quality solutions that align with team goals and project timelines.
-        Passionate about backend development and exploring opportunities in
-        Web3, I am eager to contribute to a collaborative team and continue
-        growing my expertise in the tech industry.
+        JavaScript, TypeScript, Node, Express and React, I have completed
+        multiple personal projects that showcase my skills in building dynamic,
+        responsive web applications. Experienced in Agile methodologies and
+        Scrum practices, I am adept at working in iterative, collaborative
+        environments to deliver high-quality solutions that align with team
+        goals and project timelines. Passionate about various backend
+        technologies and exploring opportunities in data science, I am eager to
+        contribute to a collaborative team and continue growing my expertise in
+        the tech industry.
       </p>
     </section>
   );

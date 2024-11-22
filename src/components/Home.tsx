e78ad83c97115417816c6db1223e391ca6a502fe
pangsui <upangsuilingeh@gmail.com>
2024-11-22 12:19:32 +0100

    text in home page edited

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,7 @@ const Home = React.forwardRef<HTMLElement, object>(() => {
 			</h1>
 			<h2>Professional Summary</h2>
 			<p>
-				I am a solution-oriented Front-End Developer with a PhD in Electrical
+				I am a solution-oriented Full-Stack Developer with a PhD in Electrical
 				Engineering, skilled in HTML, CSS, JavaScript, TypeScript, Node.js,
 				Express, and React. With a solid foundation from Wild Code School and
 				Udemy, I have developed dynamic, responsive web applications and thrive
